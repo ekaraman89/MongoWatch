bf8d8dbed2c0111b49eeed27a914f2ae801482eb
emrah karaman <ekaraman89@hotmail.com>
2021-06-11 15:05:55 +0300

    watch denemesi

--- MongoWatch/Program.cs
+++ MongoWatch/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using MongoDB.Bson;
+using MongoDB.Driver;
 
 namespace MongoWatch
 {
@@ -7,6 +9,24 @@ namespace MongoWatch
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            var client = new MongoClient("mongodb://localhost:27017");
+            var database = client.GetDatabase("db_test");
+            var collection = database.GetCollection<BsonDocument>("testColl");
+
+            var options = new ChangeStreamOptions { FullDocument = ChangeStreamFullDocumentOption.UpdateLookup };
+            var pipeline = new EmptyPipelineDefinition<ChangeStreamDocument<BsonDocument>>().Match("{ operationType: { $in: [ 'insert', 'delete' ] } }");
+
+            //var cursor = collection.Watch(pipeline, options);
+            var cursor = collection.Watch();
+
+            var enumerator = cursor.ToEnumerable().GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                ChangeStreamDocument<BsonDocument> doc = enumerator.Current;
+                // Do something here with your document
+                Console.WriteLine(doc.DocumentKey);
+            }
         }
     }
 }
