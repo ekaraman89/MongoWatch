27d287196d274d090a7407eaaa0e7391fde1007f
emrah karaman <ekaraman89@hotmail.com>
2021-06-11 14:25:29 +0300

    Add project files.

---
+++ MongoWatch/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MongoWatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
